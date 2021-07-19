07c6be548d63462c919481dcbbcc596d6d950807
shanmugabavan <shanmugabavan25621@gmail.com>
2021-07-19 23:30:37 +0530

    admin panel data displaying

--- listings/admin.py
+++ listings/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Member
 
-admin.site.register(Member)
+class Memberadmin(admin.ModelAdmin):
+    list_display=('id','name','university','age','address') #for admin panel like datebase
+    list_display_links = ('id','name')
+    list_filter = ('name',) #for filtering
+    list_editable = ('age',)
+    search_fields = ('name',)
+    list_per_page = 25
+
+admin.site.register(Member,Memberadmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import University
-admin.site.register(University)
+
+class Universityadmin(admin.ModelAdmin):
+    list_display=('id','uni_name','uni_address','phone_number',) #for admin panel like datebase
+    list_display_links = ('id','uni_name')
+    list_filter = ('uni_name',) #for filtering
+    list_editable = ('phone_number',)
+    search_fields = ('uni_name',)
+    list_per_page = 25
+admin.site.register(University,Universityadmin)
 
