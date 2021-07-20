d6a354d3a9d9b9e4c3064f6a133c2119b735a1d8
shanmugabavan <shanmugabavan25621@gmail.com>
2021-07-20 10:48:45 +0530

    display in views data fetching from backend

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from .models import Member
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return render(request,'listings/index.html')
+    members=Member.objects.all()   #fetching all from database
+
+
+    context={
+        'members':members
+    }
+    return render(request,'listings/index.html',context)
 
 def about(request):
     return render(request,'listings/about.html')
